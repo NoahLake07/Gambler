eb6dc7450bcfc164a5344296884dff5c4f2c7c8a
NoahLake07 <noahlake07@gmail.com>
2022-03-29 07:12:42 -0600

    Started Wheel Spin Algorithm

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,5 +1,6 @@
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Gambler extends GraphicsProgram {
 
@@ -7,6 +8,10 @@ public class Gambler extends GraphicsProgram {
     public int playerBalance = 1000;
     public GLabel playerBalanceLabel = new GLabel(playerBalance + " CR");
     public final int LABEL_SPACING = 10;
+    public int[] wheelSpinValues= {
+            1,100,1000,10000,0,-400,-600,-1000,750
+    };
+    public int wheelSpinLandingValue = 0;
 
     //initializing and assigning private labels
     private GLabel playerCrLabel = new GLabel("Player Credits:");
@@ -20,6 +25,10 @@ public class Gambler extends GraphicsProgram {
         add(playerCrLabel, 30,30);
         add(playerBalanceLabel, playerCrLabel.getX() + playerCrLabel.getWidth() + LABEL_SPACING,30);
         playerBalanceLabel.setVisible(true);
+    }
+
+    private void spinWheel(){
+        wheelSpinLandingValue = RandomGenerator.getInstance().nextInt(0, wheelSpinValues.length-1);
     }
 
     public static void main(String[] args) {
